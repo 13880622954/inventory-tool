a9e7554ebb9eaf0610ad337c603aff73fee06124
13880622954 <120633058@qq.com>
2026-05-14 11:03:59 +0800

    Update 条码批量生成条形码.py

--- pages/条码批量生成条形码.py
+++ pages/条码批量生成条形码.py
@@ -3,7 +3,7 @@
 使用方法：streamlit run barcode_batch_printer.py
 """
 
-import io, base64, zipfile
+1import io, base64, zipfile
 import streamlit as st
 import barcode
 from barcode.writer import ImageWriter
